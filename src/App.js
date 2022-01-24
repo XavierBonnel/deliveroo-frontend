173320169a92b9dfaf810be38be44d2aa7ddb603
XavierBonnel <x.bonnel@pm.me>
2022-01-24 09:33:05 +0100

    problem with ternary

--- src/App.js
+++ src/App.js
@@ -44,13 +44,34 @@ function App() {
           </div>
         </div>
         {/* ------------section on left------------ */}
-        <section>
-          {/* ----------section>articles for dishes---------- */}
-          {/* ----bloc of meals---- */}
-          <Meals data={data} isLoading={isLoading} />
-        </section>
-        {/* ------------aside for basket on right------------ */}
-        <aside></aside>
+        <div className="below-selected-restaurant">
+          <section>
+            {/* ----------section>articles for dishes---------- */}
+            {/* ----bloc of meals---- */}
+            <Meals data={data} isLoading={isLoading} />
+          </section>
+          {/* ------------aside for basket on right------------ */}
+          <aside>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+                ad necessitatibus soluta porro id ut quia est consequatur in,
+                velit provident magni amet laudantium voluptate possimus
+                adipisci officia delectus sunt? Perferendis consectetur facilis
+                labore! Beatae laudantium totam sapiente soluta doloremque
+                incidunt quos ad necessitatibus tempora rem aperiam doloribus,
+                maxime quibusdam alias optio suscipit odit numquam quo. Corporis
+                odio unde repellendus quos, omnis non dolores suscipit nobis
+                ipsam ea tempora distinctio nulla rem? Nisi, quisquam id? Id
+                temporibus ipsa, dolores, consectetur quos voluptas quo officia
+                officiis nemo quaerat explicabo sint laudantium, nesciunt quae
+                corrupti vitae praesentium illum dolore. Rerum expedita
+                voluptatibus laudantium? Officiis ratione mollitia enim libero
+                reiciendis numquam placeat iusto.
+              </p>
+            </div>
+          </aside>
+        </div>
       </div>
       );
     </div>
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -10,26 +10,30 @@ function Meals({ data, isLoading }) {
           {data["categories"].map((mealType, index) => {
             return (
               <div className="meals" key={index}>
-                <h3>{mealType.name}</h3>
-                <div className="meal">
-                  {mealType.meals.map((meal, index) => {
-                    return (
-                      <div key={index} className="meal-description">
-                        <div className="meal-text">
-                          <h4>{meal.title}</h4>
-                          <p>{meal.description}</p>
-                          <span>{meal.price}</span>
-                          {meal.popular && <span>Populaire</span>}
-                        </div>
-                        <div className="meal-image">
-                          {meal.picture && (
-                            <img src={meal.picture} alt="meal picture" />
-                          )}
-                        </div>
-                      </div>
-                    );
-                  })}
-                </div>
+                {mealType.meals !== [] && (
+                  <div>
+                    <h3>{mealType.name}</h3>
+                    <div className="meal">
+                      {mealType.meals.map((meal, index) => {
+                        return (
+                          <div key={index} className="meal-description">
+                            <div className="meal-text">
+                              <h4>{meal.title}</h4>
+                              <p>{meal.description}</p>
+                              <span className="price">{meal.price}€</span>
+                              {meal.popular && <span>Populaire</span>}
+                            </div>
+                            <div className="meal-image">
+                              {meal.picture && (
+                                <img src={meal.picture} alt="meal picture" />
+                              )}
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                )}
               </div>
             );
           })}
